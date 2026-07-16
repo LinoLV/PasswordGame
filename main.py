663930534d56f0aa73fb1c7c6469c8296b393869
Lino <137063881+LinoLV@users.noreply.github.com>
2026-07-16 22:41:44 +0530

    Update to 2.1

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.animation import Animation
 from kivy.core.audio import SoundLoader
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+
+Window.set_icon("icon.ico")
 
 LabelBase.register(name="NotoSans", fn_regular="NotoSans-Bold.ttf")
 
@@ -31,7 +34,7 @@ KV = '''
         halign: "center"
         font_style: "H4"
         theme_text_color: "Custom"
-        text_color: 1, 1, 1, 1
+        text_color: 0, 1, 0, 1
         font_name: "NotoSans"
         size_hint_y: None
         height: self.texture_size[1]
@@ -50,7 +53,7 @@ KV = '''
         text: "Characters: 0"
         halign: "left"
         theme_text_color: "Custom"
-        text_color: 0, 1, 0, 1
+        text_color: 1, 1, 1, 1
         font_name: "NotoSans"
         size_hint_y: None
         height: self.texture_size[1]
@@ -74,7 +77,7 @@ KV = '''
             halign: "center"
             font_style: "H3"
             theme_text_color: "Custom"
-            text_color: 1, 0, 0, 1
+            text_color: 0, 1, 0, 1
             font_name: "NotoSans"
             pos_hint: {"center_x": 0.5, "center_y": 0.6}
             opacity: 0
@@ -134,26 +137,7 @@ class PasswordApp(MDApp):
         self.error_sound = SoundLoader.load("error.mp3")
         self.complete_sound = SoundLoader.load("complete.mp3")
         self.screen.ids.password_field.bind(text=self.schedule_validation)
-        self.animate_title_rgb(self.screen.ids.game_title)
         self.last_rules_text = ""
-
-    def animate_title_rgb(self, label=None):
-        if label is None:
-            label = self.screen.ids.game_title
-        colors = [
-            (1, 0, 0, 1),
-            (0, 1, 0, 1),
-            (0, 0, 1, 1),
-            (1, 1, 0, 1),
-            (1, 0, 1, 1),
-            (0, 1, 1, 1),
-        ]
-        def next_color(i=0):
-            next_i = (i + 1) % len(colors)
-            anim = Animation(text_color=colors[next_i], duration=0.8)
-            anim.bind(on_complete=lambda *_: next_color(next_i))
-            anim.start(label)
-        next_color()
 
     def schedule_validation(self, instance, value):
         self.screen.ids.char_count.text = f"Characters: {len(value)}"
@@ -165,44 +149,99 @@ class PasswordApp(MDApp):
         label = self.screen.ids.label_message
         if not paswd:
             label.text = ""
+            self.last_rules_text = ""
             return
 
         def has_total_25(p):
             return sum(int(c) for c in p if c.isdigit()) == 25
 
         rules = []
-        if len(paswd) >= 1:
-            rules.append(("Password must contain a lowercase letter", lambda p: any(c.islower() for c in p)))
-        if len(paswd) >= 1:
-            rules.append(("Password must contain an uppercase letter", lambda p: any(c.isupper() for c in p)))
-        if len(paswd) >= 2:
-            rules.append(("Password must contain at least 7 characters", lambda p: len(p) >= 7))
-        if len(paswd) >= 7:
-            rules.append(("Password must contain a number", lambda p: any(c.isdigit() for c in p)))
-        if len(paswd) >= 7:
-            rules.append(("Password must contain the name of a month", lambda p: any(pat in p.lower() for pat in months)))
-        if len(paswd) >= 10:
-            rules.append(("Password must contain a number name between 1-10", lambda p: any(pat in p.lower() for pat in nos)))
-        if len(paswd) >= 13:
-            rules.append(("Password must contain a '#'", lambda p: "#" in p))
-        if len(paswd) >= 14:
-            rules.append(("Password must contain an '@'", lambda p: "@" in p))
-        if len(paswd) >= 14:
-            rules.append(("Password must contain a '&'", lambda p: "&" in p))
-        if len(paswd) >= 15:
-            rules.append(("The digits in the password must add up to 25", lambda p: has_total_25(p)))
-        if len(paswd) >= 16:
-            rules.append(("Password must contain an exclamation mark", lambda p: "!" in p))
-        if len(paswd) >= 17:
-            rules.append(("Password must contain a '*'", lambda p: "*" in p))
-        if len(paswd) >= 18:
-            rules.append(("Password must contain a '%'", lambda p: "%" in p))
-        if len(paswd) >= 19:
-            rules.append(("Password must contain a question mark", lambda p: "?" in p))
-        if len(paswd) >= 20:
-            rules.append(("Password must not contain a common pattern", lambda p: not any(pat in p.lower() for pat in common_patterns)))
 
-        red_rules, green_rules = [], []
+        rules.append(("Password must contain a lowercase letter",
+                  lambda p: any(c.islower() for c in p)))
+
+        if rules[-1][1](paswd):
+            rules.append(("Password must contain an uppercase letter",
+                      lambda p: any(c.isupper() for c in p)))
+
+        if len(rules) == 2 and rules[-1][1](paswd):
+            rules.append(("Password must contain at least 7 characters",
+                      lambda p: len(p) >= 7))
+
+        if len(rules) == 3 and rules[-1][1](paswd):
+            rules.append(("Password must contain a number",
+                      lambda p: any(c.isdigit() for c in p)))
+
+        if len(rules) == 4 and rules[-1][1](paswd):
+            rules.append(("Password must contain the name of a month",
+                      lambda p: any(m in p.lower() for m in months)))
+
+        if len(rules) == 5 and rules[-1][1](paswd):
+            rules.append(("Password must contain a number name between 1-10",
+                      lambda p: any(n in p.lower() for n in nos)))
+
+        if len(rules) == 6 and rules[-1][1](paswd):
+            rules.append(("Password must contain '#'",
+                      lambda p: "#" in p))
+
+        if len(rules) == 7 and rules[-1][1](paswd):
+            rules.append(("Password must contain '@'",
+                         lambda p: "@" in p))
+
+        if len(rules) == 8 and rules[-1][1](paswd):
+            rules.append(("Password must contain '&'",
+                          lambda p: "&" in p))
+
+        if len(rules) == 9 and rules[-1][1](paswd):
+            rules.append(("The digits in the password must add up to 25",
+                          lambda p: has_total_25(p)))
+
+        if len(rules) == 10 and rules[-1][1](paswd):
+            rules.append(("Password must contain '!'",
+                          lambda p: "!" in p))
+
+        if len(rules) == 11 and rules[-1][1](paswd):
+            rules.append(("Password must contain '*'",
+                          lambda p: "*" in p))
+
+        if len(rules) == 12 and rules[-1][1](paswd):
+            rules.append(("Password must contain '%'",
+                          lambda p: "%" in p))
+
+        if len(rules) == 13 and rules[-1][1](paswd):
+            rules.append(("Password must contain '?'",
+                          lambda p: "?" in p))
+
+        if len(rules) == 14 and rules[-1][1](paswd):
+            rules.append(("Password must not contain a common pattern",
+                          lambda p: not any(x in p.lower() for x in common_patterns)))
+    
+        if len(rules) == 15 and rules[-1][1](paswd):
+            rules.append(("Password must not contain spaces",
+                          lambda p: " " not in p))
+
+        if len(rules) == 16 and rules[-1][1](paswd):
+            rules.append(("Password must contain a Roman numeral (I, V, X, L, C, D or M)",
+                          lambda p: any(c in "IVXLCDM" for c in p.upper())))
+
+        if len(rules) == 17 and rules[-1][1](paswd):
+            rules.append(("Password must contain a color name",
+                          lambda p: any(color in p.lower() for color in [
+                              "red", "blue", "green", "yellow", "orange",
+                              "purple", "pink", "black", "white", "brown",
+                              "gray", "grey"
+                          ])))
+
+        if len(rules) == 18 and rules[-1][1](paswd):
+            rules.append(("Password must contain both an even and an odd digit",
+                          lambda p: any(c in "02468" for c in p) and
+                                    any(c in "13579" for c in p)))
+
+        if len(rules) == 19 and rules[-1][1](paswd):
+            rules.append(("Password length must be a multiple of 3",
+                          lambda p: len(p) % 3 == 0))
+        red_rules = []
+        green_rules = []
         all_green = True
 
         for i, (msg, check) in enumerate(rules, start=1):
@@ -212,23 +251,30 @@ class PasswordApp(MDApp):
                 red_rules.append(f"[color=FF0000]Rule {i}: {msg}[/color]")
                 all_green = False
 
-        combined = red_rules + green_rules
-        new_text = "\n\n".join(combined)
+        new_text = "\n\n".join(red_rules + green_rules)
 
         if new_text != self.last_rules_text:
-            if not all_green and self.error_sound:
-                self.error_sound.play()
-            elif all_green and self.complete_sound:
-                self.complete_sound.play()
-                Clock.schedule_once(self.show_win_screen, 1)
+            if not all_green:
+                if self.error_sound:
+                    self.error_sound.play()
+            else:
+                if self.complete_sound:
+                    self.complete_sound.play()
+                if len(rules) == 20:
+                    Clock.schedule_once(self.show_win_screen, 1)
 
-            label.opacity = 0
-            label.y = -50
-            label.text = new_text
-            anim = Animation(opacity=1, y=0, duration=0.4, t="out_back")
-            anim.start(label)
+        label.opacity = 0
+        label.y = -50
+        label.text = new_text
 
-            self.last_rules_text = new_text
+        Animation(
+            opacity=1,
+            y=0,
+            duration=0.4,
+            t="out_back"
+        ).start(label)
+
+        self.last_rules_text = new_text
 
     def show_win_screen(self, dt=None):
         self.sm.current = "win"
@@ -236,8 +282,6 @@ class PasswordApp(MDApp):
         win_sub = self.win_screen.ids.win_sub
         Animation(opacity=1, duration=1).start(win_title)
         Animation(opacity=1, duration=1.2).start(win_sub)
-        self.animate_title_rgb(win_title)
-        self.animate_title_rgb(win_sub)
 
 
 if __name__ == "__main__":
